da3870fa9d5f5365d1ac50caf1920f8f1988227e
AkshayGets <40195161+AkshayGets@users.noreply.github.com>
2020-05-14 12:16:40 +0530

    Update terraform_azure.tf

--- terraform_azure.tf
+++ terraform_azure.tf
@@ -1,16 +1,3 @@
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-    # The "feature" block is required for AzureRM provider 2.x. 
-    # If you're using version 1.x, the "features" block is not allowed.
-    version = "~>2.0"
-    features {}
-
-    subscription_id = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-    client_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-    client_secret   = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-    tenant_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-}
-
 # Create a resource group if it doesn't exist
 resource "azurerm_resource_group" "myterraformgroup" {
     name     = "myResourceGroup"
